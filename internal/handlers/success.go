a8e21cc979f37c5c5c9037cf57b3d172bea1f8ca
Arsolitt <arsolitt@gmail.com>
2025-11-20 21:20:21 +0300

    Add GlobalSuccessHandler for Telegram bot to manage success responses
    - Implemented a new GlobalSuccessHandler in the success.go file to enhance message handling upon successful command execution.
    - The handler dynamically adjusts the reply markup based on the user's state and admin status, improving user interaction.
    - Integrated logging for better debugging and tracking of success handler calls, ensuring improved feedback during command processing.

---
+++ internal/handlers/success.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"context"
+	"log/slog"
+	"slices"
+	"whitelist/internal/core"
+	"whitelist/internal/fsm"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func (h Handlers) GlobalSuccessHandler(ctx context.Context, b *bot.Bot, update *models.Update, state fsm.State, msgParams *bot.SendMessageParams) {
+	if update.Message == nil {
+		return
+	}
+	if state == fsm.StateIdle {
+		buttons := [][]models.KeyboardButton{
+			{
+				{Text: core.CommandInfo},
+				{Text: core.CommandNewWLRequest},
+			},
+		}
+		if slices.Contains(h.config.Telegram.AdminIDs, update.Message.From.ID) {
+			buttons[0] = append(buttons[0], models.KeyboardButton{Text: "Посмотреть заявки"})
+		}
+		if msgParams.ReplyMarkup == nil {
+			slog.DebugContext(ctx, "Success handler called with new markup")
+			msgParams.ReplyMarkup = &models.ReplyKeyboardMarkup{
+				Keyboard:       buttons,
+				ResizeKeyboard: true,
+			}
+		} else if msgParams.ReplyMarkup.(*models.ReplyKeyboardMarkup) != nil {
+			slog.DebugContext(ctx, "Success handler called with old markup")
+			oldMarkup := msgParams.ReplyMarkup.(*models.ReplyKeyboardMarkup)
+			msgParams.ReplyMarkup = &models.ReplyKeyboardMarkup{
+				Keyboard:              append(oldMarkup.Keyboard, buttons...),
+				ResizeKeyboard:        oldMarkup.ResizeKeyboard,
+				IsPersistent:          oldMarkup.IsPersistent,
+				OneTimeKeyboard:       oldMarkup.OneTimeKeyboard,
+				InputFieldPlaceholder: oldMarkup.InputFieldPlaceholder,
+				Selective:             oldMarkup.Selective,
+			}
+		}
+	} else if msgParams.ReplyMarkup == nil {
+		msgParams.ReplyMarkup = &models.ReplyKeyboardRemove{
+			RemoveKeyboard: true,
+			Selective:      true,
+		}
+	}
+
+	_, _ = b.SendMessage(ctx, msgParams)
+	slog.DebugContext(ctx, "Success handler called")
+}
