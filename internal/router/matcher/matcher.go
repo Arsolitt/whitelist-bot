804a889ba06d11561cc854a116ee7bdd0ee12d91
Arsolitt <arsolitt@gmail.com>
2025-11-20 21:37:13 +0300

    Enhance message matching logic in Telegram bot
    - Updated MsgText and MatchTelegramIDs functions to improve handling of nil message scenarios, ensuring robust matching for both messages and callback queries.
    - Introduced a userID variable to streamline user identification from either message or callback query, enhancing the flexibility of user ID matching.

--- internal/router/matcher/matcher.go
+++ internal/router/matcher/matcher.go
@@ -9,6 +9,9 @@ import (
 
 func MsgText(text string) bot.MatchFunc {
 	return func(update *models.Update) bool {
+		if update.Message == nil {
+			return false
+		}
 		return update.Message.Text == text
 	}
 }
@@ -47,11 +50,17 @@ func Command(cmd string) bot.MatchFunc {
 
 func MatchTelegramIDs(ids ...int64) bot.MatchFunc {
 	return func(update *models.Update) bool {
-		if update.Message == nil || update.Message.From == nil {
+		var userID int64
+		if update.Message != nil && update.Message.From != nil {
+			userID = update.Message.From.ID
+		} else if update.CallbackQuery != nil {
+			userID = update.CallbackQuery.From.ID
+		} else {
 			return false
 		}
+
 		for _, id := range ids {
-			if update.Message.From.ID == id {
+			if userID == id {
 				return true
 			}
 		}
