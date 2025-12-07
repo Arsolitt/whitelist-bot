b0ca6ccc24adbbef209c1afcda5010d49402604b
Arsolitt <arsolitt@gmail.com>
2025-12-07 20:00:48 +0300

    Refactor metastore interface and enhance functionality
    - Introduced IMetastore interface to improve abstraction and organization.
    - Added new methods for string handling in IMetastoreGetter and IMetastoreSetter.
    - Implemented ErrKeyNotFound for better error handling in metastore operations.
    - Updated TypedJSONMeta function to utilize the new IMetastoreGetter interface.

--- internal/metastore/metastore.go
+++ internal/metastore/metastore.go
@@ -3,20 +3,40 @@ package metastore
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"time"
 )
 
-type Metastore interface {
+var (
+	ErrKeyNotFound = errors.New("key not found")
+)
+
+type IMetastore interface {
+	IMetastoreGetter
+	IMetastoreSetter
+	IMetastoreDeleter
+}
+
+type IMetastoreGetter interface {
 	Get(ctx context.Context, uniqueID string, key string) ([]byte, error)
-	Set(ctx context.Context, uniqueID string, key string, value any) error
-	SetWithTTL(ctx context.Context, uniqueID string, key string, value any, ttl time.Duration) error
-	Delete(ctx context.Context, uniqueID string, key string) error
+	GetString(ctx context.Context, uniqueID string, key string) (string, error)
 	Exists(ctx context.Context, uniqueID string, key string) (bool, error)
 }
 
-func TypedJSONMeta[T any](ctx context.Context, metastore Metastore, uniqueID string, key string) (T, error) {
+type IMetastoreSetter interface {
+	Set(ctx context.Context, uniqueID string, key string, value any) error
+	SetString(ctx context.Context, uniqueID string, key string, value string) error
+	SetWithTTL(ctx context.Context, uniqueID string, key string, value any, ttl time.Duration) error
+	SetStringWithTTL(ctx context.Context, uniqueID string, key string, value string, ttl time.Duration) error
+}
+
+type IMetastoreDeleter interface {
+	Delete(ctx context.Context, uniqueID string, key string) error
+}
+
+func TypedJSONMeta[T any](ctx context.Context, mg IMetastoreGetter, uniqueID string, key string) (T, error) {
 	var zero T
-	data, err := metastore.Get(ctx, uniqueID, key)
+	data, err := mg.Get(ctx, uniqueID, key)
 	if err != nil {
 		return zero, err
 	}
